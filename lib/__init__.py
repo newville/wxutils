3278912bfd01a189911a27c869be8dfe144f1897
Matthew Newville <newville@cars.uchicago.edu>
2017-04-02 13:05:22 -0500

    add readlinetextctrl

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__  = 'Matthew Newville'
 
 import sys
-if not hasattr(sys, 'frozen'):
-    try:
-        import wxversion
-        wxversion.ensureMinimal('2.8')
-    except:
-        pass
 import wx
 
 from .debugtime import debugtime
@@ -18,8 +12,6 @@ from .dates import hms, DateTimeCtrl
 from .files import fix_filename, FileOpen, FileSave, SelectWorkdir
 from .icons import get_icon
 
-from .periodictable import  PeriodicTablePanel
-
 from .utils import (GUIColors, YesNo, Check, GridPanel, RowPanel,
                    SimpleText, TextCtrl, LabeledTextCtrl,
                    HyperText, EditableListBox, Font, SetChildrenFont,
@@ -27,3 +19,6 @@ from .utils import (GUIColors, YesNo, Check, GridPanel, RowPanel,
                    OkCancel, set_sizer, pack, Popup, EmptyBitmap)
 
 from .utils import RIGHT, LEFT, CEN, LCEN, RCEN, CCEN, LTEXT, FRAMESTYLE
+
+from .readlinetextctrl import ReadlineTextCtrl
+from .periodictable import  PeriodicTablePanel
