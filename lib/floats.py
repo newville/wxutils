9d8edd17ec66d51541d2ae855ffc7ffaa30ecab8
Matthew Newville <newville@cars.uchicago.edu>
2018-04-06 17:31:45 -0500

    add FloatSpin, with tooltip and action

--- lib/floats.py
+++ lib/floats.py
@@ -5,6 +5,7 @@ Code for floating point controls
 import sys
 from functools import partial
 import wx
+from wx.lib.agw.floatspin import FloatSpin, EVT_FLOATSPIN
 
 HAS_NUMPY = False
 try:
@@ -12,6 +13,21 @@ try:
     HAS_NUMPY = True
 except ImportError:
     pass
+
+def FloatSpin(parent, value=None, action=None, tooltip=None,
+                 size=(100, -1), digits=1, increment=1, **kws):
+    """FloatSpin with action"""
+    fs = FloatSpin(parent, -1, value, size=size, digits=digits,
+                   increment=increment, **kws)
+    if action is not None:
+        fs.Bind(wx.EVT_FLOATSPIN, action)
+    if tooltip is not None:
+        if is_wxPhoenix:
+            fs.SetToolTip(tooltip)
+        else:
+            fs.SetToolTipString(tooltip)
+    return fs
+
 
 def make_steps(prec=3, tmin=0, tmax=10, base=10, steps=(1, 2, 5)):
     """make a list of 'steps' to use for a numeric ComboBox
