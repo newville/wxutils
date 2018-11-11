20e0708ee5c988f817dab33c489d7e201dde33f5
Matthew Newville <newville@cars.uchicago.edu>
2018-11-11 11:13:46 -0600

    more updates to floatspin, now allowing a 'pin' for interactive work

--- lib/floats.py
+++ lib/floats.py
@@ -16,19 +16,7 @@ try:
 except ImportError:
     pass
 
-def FloatSpin(parent, value=None, action=None, tooltip=None,
-                 size=(100, -1), digits=1, increment=1, **kws):
-    """FloatSpin with action"""
-    fs = fspin.FloatSpin(parent, -1, value, size=size, digits=digits,
-                   increment=increment, **kws)
-    if action is not None:
-        fs.Bind(fspin.EVT_FLOATSPIN, action)
-    if tooltip is not None:
-        if is_wxPhoenix:
-            fs.SetToolTip(tooltip)
-        else:
-            fs.SetToolTipString(tooltip)
-    return fs
+from .icons import get_icon
 
 def make_steps(prec=3, tmin=0, tmax=10, base=10, steps=(1, 2, 5)):
     """make a list of 'steps' to use for a numeric ComboBox
@@ -290,9 +278,7 @@ class FloatCtrl(wx.TextCtrl):
                 elif self.even_only and (val %2 == 1):
                     self.is_valid = False
                     val = val + 1
-
         except:
-            print(sys.exc_info)
             self.is_valid = False
         self.__bound_val = self.__val = val
         fgcol, bgcol = self.fgcol_valid, self.bgcol_valid
@@ -302,6 +288,38 @@ class FloatCtrl(wx.TextCtrl):
         self.SetForegroundColour(fgcol)
         self.SetBackgroundColour(bgcol)
         self.Refresh()
+
+
+def FloatSpin(parent, value=0, action=None, tooltip=None,
+                 size=(100, -1), digits=1, increment=1, **kws):
+    """FloatSpin with action and tooltip"""
+    if value is None:
+        value = 0
+    fs = fspin.FloatSpin(parent, -1, size=size, value=value,
+                         digits=digits, increment=increment, **kws)
+    if action is not None:
+        fs.Bind(fspin.EVT_FLOATSPIN, action)
+    if tooltip is not None:
+        if is_wxPhoenix:
+            fs.SetToolTip(tooltip)
+        else:
+            fs.SetToolTipString(tooltip)
+    return fs
+
+def FloatSpinWithPin(parent, value=0, pin_action=None, **kws):
+    """create a FloatSpin with Pin button with action"""
+    tooltip = 'use last point selected from plot'
+    fspin = FloatSpin(parent, value=value, **kws)
+    bmbtn = wx.BitmapButton(parent, id=-1, bitmap=get_icon('pin'),
+                            size=(25, 25))
+    if pin_action is not None:
+        parent.Bind(wx.EVT_BUTTON, pin_action, bmbtn)
+    if is_wxPhoenix:
+        bmbtn.SetToolTip(tooltip)
+    else:
+        bmbtn.SetToolTipString(tooltip)
+    return fspin, bmbtn
+
 
 class NumericCombo(wx.ComboBox):
     """
