090fe40db76ba845fa0c52818c8caf2dd8e2beb4
Matthew Newville <newville@cars.uchicago.edu>
2018-11-11 13:15:23 -0600

    add SavedParameterDialog

--- lib/dialogs.py
+++ lib/dialogs.py
@@ -5,6 +5,11 @@ file utilities
 import os
 import sys
 import wx
+from collections import namedtuple
+
+from .text import SimpleText
+from .gridpanel import GridPanel
+from .paths import get_configfile, save_configfile
 
 if sys.version[0] == '2':
     from string import maketrans
@@ -105,3 +110,53 @@ def OkCancel(panel, onOK=None, onCancel=None):
     btnsizer.AddButton(_no)
     btnsizer.Realize()
     return btnsizer
+
+class SavedParameterDialog(wx.Dialog):
+    """dialog to prompt for string with
+    read/write of previous value to config file
+
+    dlg = SavedParameterDialog(label='Prefix',
+                               title='Parameter Prompt',
+                               configfile='.myparam.dat')
+    res = dlg.GetResponse()
+    dlg.Destroy()
+    if res.ok:
+        prefix = res.value
+
+    """
+    def __init__(self, label='Value:', title='Parameter Prompt',
+                 configfile=None, **kws):
+        wx.Dialog.__init__(self, None, wx.ID_ANY, title=title)
+
+        value = ''
+        if configfile is not None:
+            cfile = get_configfile(configfile)
+            if cfile is not None:
+                with open(cfile, 'r') as fh:
+                    self.configfile = configfile
+                    for line in fh.readlines():
+                        line = line.strip()[:-1]
+                        if line.startswith('#') or len(line)<1:
+                            continue
+                        value = line
+                        break
+
+
+        self.text = wx.TextCtrl(self, value=value, size=(250, -1))
+        panel = GridPanel(self, ncols=3, nrows=4, pad=2)
+        panel.Add(wx.StaticText(self, label=label), newrow=True)
+        panel.Add(self.text)
+        panel.Add(OkCancel(self), dcol=2, newrow=True)
+        panel.pack()
+
+    def GetResponse(self, master=None, gname=None, ynorm=True):
+        self.Raise()
+        response = namedtuple('Reponse', ('ok', 'value'))
+        ok, value = False, None
+        if self.ShowModal() == wx.ID_OK:
+            value = self.text.GetValue()
+            ok = True
+        if self.configfile is not None:
+            tout = '# saved %s\n%s\n\n' % (time.ctime(), value)
+            save_configfile(self.configfile, tout)
+        return response(ok, value)
