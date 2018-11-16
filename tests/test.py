39ff14214b9fa0c14da16f8d5ccf6013f889ca44
Matthew Newville <newville@cars.uchicago.edu>
2018-11-15 21:10:47 -0600

    update tests to import more things from wwutils

--- tests/test.py
+++ tests/test.py
@@ -4,3 +4,10 @@ def test_1():
     import wxutils
 
     assert('Check' in dir(wxutils))
+
+    from wxutils import (Button, CEN, Check, Choice, EditableListBox,
+                         FRAMESTYLE, FileOpen, FileSave, FloatCtrl, Font,
+                         GUIColors, GridPanel, HLine, HyperText, LCEN,
+                         LEFT, MenuItem, Popup, RCEN, RIGHT, RowPanel,
+                         SimpleText, TextCtrl, fix_filename, get_icon,
+                         pack)
