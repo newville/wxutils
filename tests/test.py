fcbf27c04e06718b0f876e314ff19840d5ac37bb
Matthew Newville <newville@cars.uchicago.edu>
2018-11-15 21:24:19 -0600

    update tests

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,19 @@
 import wx
+import time
+import unittest
+import pytest
 
-def test_1():
-    import wxutils
+from wxutils import (Button, CEN, Check, Choice, EditableListBox,
+                     FRAMESTYLE, FileOpen, FileSave, FloatCtrl, Font,
+                     GUIColors, GridPanel, HLine, HyperText, LCEN, LEFT,
+                     MenuItem, Popup, RCEN, RIGHT, RowPanel, SimpleText,
+                     TextCtrl, fix_filename, get_icon, pack)
 
-    assert('Check' in dir(wxutils))
+class TestCase(unittest.TestCase):
+    def test_imports(self):
+        import wxutils
 
-    from wxutils import (Button, CEN, Check, Choice, EditableListBox,
-                         FRAMESTYLE, FileOpen, FileSave, FloatCtrl, Font,
-                         GUIColors, GridPanel, HLine, HyperText, LCEN,
-                         LEFT, MenuItem, Popup, RCEN, RIGHT, RowPanel,
-                         SimpleText, TextCtrl, fix_filename, get_icon,
-                         pack)
+        assert('Check' in dir(wxutils))
+
+if __name__ == '__main__':
+    pytest.main(['-v', '-x', '-s'])
