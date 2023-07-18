789bbf4c18c6c6de236aa8a3f8475f645edb2e14
Matt Newville <newville@cars.uchicago.edu>
2023-07-17 21:16:11 -0500

    some gtk3-specific tweaks

--- wxutils/floats.py
+++ wxutils/floats.py
@@ -290,7 +290,7 @@ class FloatCtrl(wx.TextCtrl):
 
 
 def FloatSpin(parent, value=0, action=None, tooltip=None, size=(100, -1),
-              digits=1, increment=1, use_gtk3=False, **kws):
+              digits=1, increment=1, use_gtk3=True, **kws):
     """FloatSpin with action and tooltip"""
     if value is None:
         value = 0
@@ -302,9 +302,9 @@ def FloatSpin(parent, value=0, action=None, tooltip=None, size=(100, -1),
     if use_gtk3 and 'gtk3' in wx.PlatformInfo:
         maxval = kws.pop('max_val', None)
         minval = kws.pop('min_val', None)
-        fmt = "%%%df" % digits
+        fmt  = "%%%df" % digits
         fs = wx.SpinCtrlDouble(parent, -1, value=fmt % value,
-                               size=(size[0]+40, size[1]),
+                               size=(size[0]+30, size[1]),
                                inc=increment, **kws)
         fs.SetDigits(digits)
         if minval is not None:
@@ -329,9 +329,12 @@ def FloatSpin(parent, value=0, action=None, tooltip=None, size=(100, -1),
 def FloatSpinWithPin(parent, value=0, pin_action=None,
                      tooltip='select point from plot', **kws):
     """create a FloatSpin with Pin button with action"""
+    bsize = (25, 25)
+    if 'gtk3' in wx.PlatformInfo:
+        bsize = (35, 35)
     fspin = FloatSpin(parent, value=value, **kws)
     bmbtn = wx.BitmapButton(parent, id=-1, bitmap=get_icon('pin'),
-                            size=(25, 25))
+                            size=bsize)
     if pin_action is not None:
         parent.Bind(wx.EVT_BUTTON, pin_action, bmbtn)
     bmbtn.SetToolTip(tooltip)
